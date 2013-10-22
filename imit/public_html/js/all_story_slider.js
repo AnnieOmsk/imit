26b112a5162975d5966462de3c164866ac01da32
sevenbits <sevenbits@sevenbits-H55M-D2H.(none)>
2013-10-22 17:53:57 +0700

    index and people

---
+++ imit/public_html/js/all_story_slider.js
@@ -0,0 +1,117 @@
+$(document).ready(function(){
+    hideAllStories();
+    $('#story_1').click(function (e) {
+        if($("#all-story_1").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_1").show();
+        } else {
+            $("#all-story_1").hide();
+        }
+    });
+    $('#story_2').click(function (e) {
+      if($("#all-story_2").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_2").show();
+        } else {
+            $("#all-story_2").hide();
+        }
+    });
+    $('#story_3').click(function (e) {
+      if($("#all-story_3").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_3").show();
+        } else {
+            $("#all-story_3").hide();
+        }
+    });
+    $('#story_4').click(function (e) {
+      if($("#all-story_4").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_4").show();
+        } else {
+            $("#all-story_4").hide();
+        }
+    });
+    $('#story_5').click(function (e) {
+      if($("#all-story_5").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_5").show();
+        } else {
+            $("#all-story_5").hide();
+        }
+    });
+    $('#story_6').click(function (e) {
+      if($("#all-story_6").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_6").show();
+        } else {
+            $("#all-story_6").hide();
+        }
+    });
+    $('#story_7').click(function (e) {
+      if($("#all-story_7").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_7").show();
+        } else {
+            $("#all-story_7").hide();
+        }
+    });
+    $('#story_8').click(function (e) {
+      if($("#all-story_8").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_8").show();
+        } else {
+            $("#all-story_8").hide();
+        }
+    });
+    $('#story_9').click(function (e) {
+      if($("#all-story_9").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_9").show();
+        } else {
+            $("#all-story_9").hide();
+        }
+    });
+    $('#story_10').click(function (e) {
+      if($("#all-story_10").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_10").show();
+        } else {
+            $("#all-story_10").hide();
+        }
+    });
+    $('#story_11').click(function (e) {
+      if($("#all-story_11").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_11").show();
+        } else {
+            $("#all-story_11").hide();
+        }
+    });
+    $('#story_12').click(function (e) {
+      if($("#all-story_12").css('display') =="none") {
+            hideAllStories();
+            $("#all-story_12").show();
+        } else {
+            $("#all-story_12").hide();
+        }
+    });
+});
+
+function hideAllStories() {
+    $("#all-story_1").hide();
+    $("#all-story_2").hide();
+    $("#all-story_3").hide();
+    $("#all-story_4").hide();
+    $("#all-story_5").hide();
+    $("#all-story_6").hide();
+    $("#all-story_7").hide();
+    $("#all-story_8").hide();
+    $("#all-story_9").hide();
+    $("#all-story_10").hide();
+    $("#all-story_11").hide();
+    $("#all-story_12").hide();
+}
+
+
+
---
+++ imit/public_html/js/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#myController").jFlow({
+        controller: ".jFlowControl", // must be class, use . sign
+	slideWrapper : "#jFlowSlider", // must be id, use # sign
+	slides: "#slider",  // the div where all your sliding divs are nested in
+	selectedWrapper: "jFlowSelected",  // just pure text, no sign
+	width: "820px",  // this is the width for the content-slider
+	height: "513px",  // this is the height for the content-slider
+	duration: 400,  // time in miliseconds to transition one slide
+	prev: ".left-slide", // must be class, use . sign
+	next: ".right-slide", // must be class, use . sign
+	auto: true	
+    });
+});
+
