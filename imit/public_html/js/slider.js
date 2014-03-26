65db11eea2f11cbc729a8aa63308c19b5228662f
Sibert Dmitry <dimaaasik.s@gmail.com>
2014-03-26 19:39:01 +0700

    Added new fade in\out slider

--- imit/public_html/js/slider.js
+++ imit/public_html/js/slider.js
@@ -1,15 +1,13 @@
+//bxSlider 4.1.1
+//For complete documentation, tons of examples, and a good time, visit:
+//[http://bxslider.com] (http://bxslider.com)
+//Written by: Steven Wanderski
+//License
+//Released under the MIT license - http://opensource.org/licenses/MIT
 $(document).ready(function(){
-    $("#myController").jFlow({
-        controller: ".jFlowControl", // must be class, use . sign
-	slideWrapper : "#jFlowSlider", // must be id, use # sign
-	slides: "#slider",  // the div where all your sliding divs are nested in
-	selectedWrapper: "jFlowSelected",  // just pure text, no sign
-	width: "820px",  // this is the width for the content-slider
-	height: "513px",  // this is the height for the content-slider
-	duration: 400,  // time in miliseconds to transition one slide
-	prev: ".left-slide", // must be class, use . sign
-	next: ".right-slide", // must be class, use . sign
-	auto: false	
-    });
+	$('.bxslider').bxSlider({
+    mode: 'fade',
+    speed: 500,
+  });
 });
 
