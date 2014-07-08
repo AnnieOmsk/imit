aa9f59010f2d74d5bfa0b4a47e6c71e5bdb523e3
dmitry <dimaaasik.s@gmail.com>
2014-07-08 11:34:11 +0700

    Added full story closing on missclick on graduates page/

--- imit/public_html/js/component/show_full_story_component.js
+++ imit/public_html/js/component/show_full_story_component.js
@@ -7,6 +7,11 @@ function() {
     var offsetTop = 200;
     var currentElementTop = 0;
 
+    this.defaultAttrs({
+        closeButton: ".story-close",
+        componentShadow: ".story-container-shadow"
+    })
+
     this.showFullStory = function(event, data) {
         prevOpenStoryId = data['id'];
         this.$node.find(prevOpenStoryId).css('display', 'inline-block');
@@ -31,6 +36,7 @@ function() {
 
     this.after('initialize', function() {
         this.on(document, 'click-preview-story', this.showFullStory);
-        this.on('.story-close', 'click', this.hideFullStory);
+        this.on(this.attr.closeButton, 'click', this.hideFullStory);
+        this.on(this.attr.componentShadow, 'click', this.hideFullStory);
     });
 });
