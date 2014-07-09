20a6adffe91defef8d6fd9de6167e838fd781dc6
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-07-09 17:27:11 +0700

    Express-app made from static template

--- imit-new/routes/index.js
+++ imit-new/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/index.html');
+});
+
+router.get('/index.html', function(req, res) {
+  res.render('home/index', { title: 'IMIT Index' });
+});
+
+router.get('/contacts.html', function(req, res) {
+  res.render('home/contacts', { title: 'IMIT Contacts' });
+});
+
+router.get('/go_to_university.html', function(req, res) {
+  res.render('home/go_to_university', { title: 'IMIT Go to university' });
+});
+
+router.get('/graduates.html', function(req, res) {
+  res.render('home/graduates', { title: 'IMIT Graduates' });
+});
+
+router.get('/interesting.html', function(req, res) {
+  res.render('home/interesting', { title: 'IMIT Interesting' });
 });
 
 module.exports = router;
