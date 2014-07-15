a7ddaf280f6831c6f1215fc6d5227f89472b1bc1
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-07-15 11:01:47 +0700

    Application configuration was refactored

--- imit/app.js
+++ imit/app.js
@@ -1,25 +1,13 @@
 var express = require('express');
-var path = require('path');
-var middleware = require('./configuration/middleware');
+var webconfig = require('./configuration/webconfig');
 var routes = require('./configuration/routes');
-var custom = require('./configuration/custom');
 var errors = require('./configuration/errors');
 var security = require('./configuration/security');
 
 var app = express();
 
-// Middleware configuration
-middleware.init(app);
-
-// Setting up static resources directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Setting up template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// Customize chain
-custom.init(app);
+// App main web configuration
+webconfig.init(app);
 
 // Initialize security
 security.init(app);
--- imit/configuration/webconfig.js
+++ imit/configuration/webconfig.js
@@ -1,11 +1,14 @@
 /**
- * Middleware configuration
+ * Web configuration
+ * Initializing all middleware, static files and views
  */
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var path = require('path');
+var static = require('express').static;
 var csrf = require('csurf');
 var RedisStore = require('connect-redis')(session);
 var settings = require('./settings');
@@ -13,7 +16,7 @@ var settings = require('./settings');
 module.exports = {
 
   /**
-   * Initialize middleware configuration
+   * Setting up all middleware, static files and views
    * @param app Express app
    */
   init: function(app) {
@@ -29,5 +32,21 @@ module.exports = {
       resave: true
     }));
     app.use(csrf());
+
+    // Setting up static resources directory
+    app.use(static(path.join(__dirname, '..', 'public')));
+
+    // Setting up template engine
+    app.set('views', path.join(__dirname, '..', 'views'));
+    app.set('view engine', 'jade');
+
+    // Add empty often used objects to avoid undefined errors
+    app.locals.errors = {};
+
+    // Add csrf token to view
+    app.use(function(req, res, next) {
+      res.locals['_csrf'] = req.csrfToken();
+      next();
+    });
   }
 };
