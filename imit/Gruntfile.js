d12a4803a122cf483ad5af5f84925469807c31e1
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-07-24 19:01:14 +0700

    Added minifying script

---
+++ imit/Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // configure the tasks
+  grunt.initConfig({
+    clean: {
+      dev: {
+        src: [ 'views/layouts', 'public/css', 'public/js' ]
+      }
+    },
+    multiply_layouts: {
+      dev: {
+        options: {
+          staticSrc: 'public-dev',
+          staticDst: 'public',
+          cssDir: 'css',
+          jsDir: 'js'
+        },
+        layouts: [{
+          src: 'views/layouts-dev',
+          dst: 'views/layouts',
+          mode: 'dev'
+        }]
+      },
+      prod: {
+        options: {
+          staticSrc: 'public-dev',
+          staticDst: 'public',
+          cssDir: 'css',
+          jsDir: 'js'
+        },
+        layouts: [{
+          src: 'views/layouts-dev',
+          dst: 'views/layouts',
+          mode: 'prod'
+        }]
+      }
+    }
+  });
+
+  // load the tasks
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-multiply-layouts');
+
+  // define the tasks
+
+  grunt.registerTask('dev', 'Production task.', ['clean', 'multiply_layouts:dev']);
+  grunt.registerTask('default', 'Development task.', [ 'clean', 'multiply_layouts:prod']);
+};
