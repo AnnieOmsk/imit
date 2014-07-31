f16e5be4776c221a384790561390ecbcd011bc26
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-07-31 11:34:32 +0700

    Tests fixed

--- imit/tests/services/admin-int.js
+++ imit/tests/services/admin-int.js
@@ -8,7 +8,7 @@ var util = require('util');
 describe('Admin service tests',function(){
 
   it('There should not be any error', function(done){
-    var promise = service.findAdmin("test@test", "password");
+    var promise = service.findAdmin("test@test");
     promise.then(function(res) {
       console.log(util.inspect(res));
       assert.equal(null, res);
